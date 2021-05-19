e3e4d028a325fa68418ee9fe023f0e5750c0720a
bethanyuo <bethanyosueke@gmail.com>
2021-05-19 05:25:17 -0500

    moralis initialization

---
+++ main.js
@@ -0,0 +1,37 @@
+
+Moralis.initialize("SnYcyIpHJXpRwS4esU7uPq1ghuJXTd339TQpW5Pp");
+
+Moralis.serverURL = 'https://ymvrtxschj2j.moralis.io:2053/server';
+
+init = async () => {
+    window.web3 = await Moralis.Web3.enable();
+    initUser();
+}
+
+initUser = async () => {
+    if (await Moralis.User.current()) {
+        hideElement(userConnectButton);
+        showElement(userProfileButton);
+    } else {
+        showElement(userConnectButton);
+        hideElement(userProfileButton);
+    }
+}
+
+login = async () => {
+    try {
+        await Moralis.Web3.authenticate();
+        initUser();
+    } catch (error) {
+        alert(error);
+    }
+}
+
+hideElement = (element) => element.style.display = "none";
+showElement = (element) => element.style.display = "block";
+
+const userConnectButton = document.getElementById("btnConnect");
+userConnectButton.onclick = login;
+const userProfileButton = document.getElementById("btnUserInfo");
+
+init();

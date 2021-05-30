19a0fb6e49a0a8bddc63cb52810901c16b6edb66
bethanyuo <bethanyosueke@gmail.com>
2021-05-30 07:28:40 -0500

    renderItems && ChefMarketPlace contract

--- frontend/main.js
+++ frontend/main.js
@@ -1,15 +1,16 @@
 //const Moralis  = require('moralis/node');
 
-Moralis.initialize( "SnYcyIpHJXpRwS4esU7uPq1ghuJXTd339TQpW5Pp" );
-Moralis.serverURL = 'https://ymvrtxschj2j.moralis.io:2053/server';
+Moralis.initialize( "kaBvvlEVwlot4E7VelBssdgDVmlDTyD6rykYzrr9" );
+Moralis.serverURL = 'https://hmdky9gx8kwa.moralis.io:2053/server';
 
-const TOKEN_CONTRACT_ADDRESS = "0x09fd2c224a50d2c090bB20Ee535d8e8A6Fe14cC7";
+const TOKEN_CONTRACT_ADDRESS = "0x8EFCE4007f94f122b8EfeF0A0a249Ce0318D87c2";
 
 init = async () => {
+    hideElement( userItemSection );
     hideElement( userInfo );
     hideElement( createItemForm );
     window.web3 = await Moralis.Web3.enable();
-    window.tokenContract = new web3.eth.Contract(tokenContractABI, TOKEN_CONTRACT_ADDRESS);
+    window.tokenContract = new web3.eth.Contract( tokenContractABI, TOKEN_CONTRACT_ADDRESS );
     initUser();
 }
 
@@ -18,10 +19,13 @@ initUser = async () => {
         hideElement( userConnectButton );
         showElement( userProfileButton );
         showElement( openCreateItemButton );
+        showElement( openUserItemsButton );
+        loadUserItems();
     } else {
         showElement( userConnectButton );
         hideElement( userProfileButton );
         hideElement( openCreateItemButton );
+        hideElement( openUserItemsButton );
 
     }
 }
@@ -62,6 +66,41 @@ openUserInfo = async () => {
     } else {
         login();
     }
+}
+
+loadUserItems = async () => {
+    const ownedItems = await Moralis.Cloud.run( "getUserItems" );
+    ownedItems.forEach( item => {
+        getAndRenderItemData( item, renderUserItem );
+    } )
+}
+
+initTemplate = ( id ) => {
+    const template = document.getElementById( id );
+    template.id = "";
+    template.parentNode.removeChild( template );
+    return template;
+}
+
+renderUserItem = ( item ) => {
+    const userItem = userItemTemplate.cloneNode( true );
+    userItem.getElementsByTagName( "img" )[0].src = item.image;
+    userItem.getElementsByTagName( "img" )[0].alt = item.name;
+    userItem.getElementsByTagName( "h5" )[0].innerText = item.name;
+    userItem.getElementsByTagName( "p" )[0].innerText = item.description;
+    userItems.appendChild( userItem );
+}
+
+getAndRenderItemData = ( item, renderFunction ) => {
+    fetch( item.tokenUri )
+        .then( res => res.json() )
+        .then( data => {
+            data.symbol = item.symbol;
+            data.tokenId = item.tokenId;
+            data.tokenAddress = item.tokenAddress;
+            renderFunction( data );
+        } )
+
 }
 
 saveUserInfo = async () => {
@@ -105,29 +144,38 @@ createItem = async () => {
     const nftFileMetadataFilePath = nftFileMetadataFile.ipfs();
     const nftFileMetadataFileHash = nftFileMetadataFile.hash();
 
-    const nftId = await mintNft(nftFileMetadataFilePath);
+    const nftId = await mintNft( nftFileMetadataFilePath );
 
     // Simple syntax to create a new subclass of Moralis.Object.
     const Item = Moralis.Object.extend( "Item" );
     // Create a new instance of that class.
     const item = new Item();
 
-    item.set('name', createItemNameField.value);
-    item.set('description', createItemDescriptionField.value);
-    item.set('nftFilePath', nftFilePath);
-    item.set('nftFileHash', nftFileHash);
-    item.set('metadataFilePath', nftFileMetadataFilePath);
-    item.set('metadataFileHash', nftFileMetadataFileHash);
-    item.set('nftId', nftId);
-    item.set('nftContractAddrss', TOKEN_CONTRACT_ADDRESS);
+    item.set( 'name', createItemNameField.value );
+    item.set( 'description', createItemDescriptionField.value );
+    item.set( 'nftFilePath', nftFilePath );
+    item.set( 'nftFileHash', nftFileHash );
+    item.set( 'metadataFilePath', nftFileMetadataFilePath );
+    item.set( 'metadataFileHash', nftFileMetadataFileHash );
+    item.set( 'nftId', nftId );
+    item.set( 'nftContractAddress', TOKEN_CONTRACT_ADDRESS );
     await item.save();
-    console.log(item);
+    console.log( item );
 }
 
-mintNft = async (metadataUrl) => {
-    const receipt = await tokenContract.methods.createItem(metadataUrl).send({from: ethereum.selectedAddress});
-    console.log(receipt);
+mintNft = async ( metadataUrl ) => {
+    const receipt = await tokenContract.methods.createItem( metadataUrl ).send( { from: ethereum.selectedAddress } );
+    console.log( receipt );
     return receipt.events.Transfer.returnValues.tokenId;
+}
+
+openUserItems = async () => {
+    user = await Moralis.User.current();
+    if ( user ) {
+        showElement( userItemSection );
+    } else {
+        login();
+    }
 }
 
 hideElement = ( element ) => element.style.display = 'none';
@@ -167,7 +215,16 @@ const createItemStatusField = document.getElementById( "selectCreateItemStatus" 
 const createItemFile = document.getElementById( "fileCreateItemFile" );
 
 
-document.getElementById( "btnCloseCreateItem" ).onclick = hideElement( createItemForm );
+document.getElementById( "btnCloseCreateItem" ).onclick = () => hideElement( createItemForm );
 document.getElementById( "btnCreateItem" ).onclick = createItem;
 
+// User Items
+
+const userItemSection = document.getElementById( "userItems" );
+const userItems = document.getElementById( "userItemsList" );
+document.getElementById( "btnCloseUserItems" ).onclick = () => hideElement( userItemSection );
+const openUserItemsButton = document.getElementById( "btnMyItems" );
+openUserItemsButton.onclick = openUserItems;
+
+const userItemTemplate = initTemplate( "itemTemplate" );
 init();
